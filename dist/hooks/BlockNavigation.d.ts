75c9b6e5673daedd1bfc91862f53181ecd713bc6
Nikola Begedin <begedinnikola@gmail.com>
2022-06-06 07:26:42 +0200

    npm run build

---
+++ dist/hooks/BlockNavigation.d.ts
@@ -0,0 +1,6 @@
+import { ViewHook } from './types';
+/**
+ * Handles block navigation by binding to key events and dispatching navigation
+ * events to the backend
+ */
+export declare const BlockNavigation: ViewHook;
